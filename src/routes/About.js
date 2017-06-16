bb9c3b70ce62748e09610ddf5f5e223158c26a65
Sarah Sauseng <sarahsauseng@Sarahs-MacBook-Pro.local>
2017-06-16 17:40:10 +0200

    ADD style to about and profile.

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,41 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import { Paper } from 'material-ui';
 import pkg from '../../package.json';
 
+const styles = {
+  head: {
+    width: '100%',
+    backgroundColor: '#27c79a',
+    padding: '16px',
+    height: '70px',
+  },
+  button: {
+    float: 'right',
+    backgroundColor: '#333e50',
+  },
+  h3: {
+    float: 'left',
+    lineHeight: 1,
+    margin: 10,
+    textTransform: 'lowercase',
+    letterSpacing: '0.1em',
+  },
+  span: {
+    color: '#333e50',
+    fontWeight: 'bold',
+  },
+  loading: {
+    fill: '#27c79a',
+    color: '#27c79a',
+    textAlign: 'center',
+    margin: 'auto',
+    width: '100%',
+  },
+  list: {
+    paddingLeft: 0,
+    marginTop: 25,
+  },
+};
 
 export default class About extends React.Component {
 
@@ -13,6 +47,12 @@ export default class About extends React.Component {
   }
 
   render() {
-    return <div><h2>About GameFAM ${this.state.version}</h2></div>;
+    return <div>
+        <Paper style={ styles.head } rounded={false} zDepth={1}>
+          <h3 style={ styles.h3 }>About <span style={ styles.span }>
+                gamefam</span> - { this.state.version }
+          </h3>
+        </Paper>
+    </div>;
   }
 }
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Paper } from 'material-ui';
 import { connect } from 'react-redux';
 
+const styles = {
+  head: {
+    width: '100%',
+    backgroundColor: '#27c79a',
+    padding: '16px',
+    height: '70px',
+  },
+  button: {
+    float: 'right',
+    backgroundColor: '#333e50',
+  },
+  h3: {
+    float: 'left',
+    lineHeight: 1,
+    margin: 10,
+    textTransform: 'lowercase',
+    letterSpacing: '0.1em',
+  },
+  span: {
+    color: '#333e50',
+    fontWeight: 'bold',
+  },
+  loading: {
+    fill: '#27c79a',
+    color: '#27c79a',
+    textAlign: 'center',
+    margin: 'auto',
+    width: '100%',
+  },
+  list: {
+    paddingLeft: 0,
+    marginTop: 25,
+  },
+};
+
 @connect(store => ({
-  user: store.user,
+  user: store.user.user_data,
 }))
 
 export default class Profile extends React.Component {
@@ -11,11 +47,14 @@ export default class Profile extends React.Component {
     user: PropTypes.object,
   };
 
-  componentWillMount() {
-
-  }
 
   render() {
-    return <h2>Profile</h2>;
+    return <div>
+      <Paper style={ styles.head } rounded={false} zDepth={1}>
+          <h3 style={ styles.h3 }><span style={ styles.span }>
+              { this.props.user.name }</span>'s Profile
+          </h3>
+        </Paper>
+    </div>;
   }
 }
