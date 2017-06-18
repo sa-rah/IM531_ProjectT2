528fb19e5a87967941eb405f996972696e141de7
Sarah Sauseng <sarahsauseng@Sarahs-MacBook-Pro.local>
2017-06-18 14:56:09 +0200

    EDIT homepage.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Paper } from 'material-ui';
+import UseIcon from 'material-ui/svg-icons/action/question-answer';
+import WhatIcon from 'material-ui/svg-icons/action/info';
 
 const styles = {
   head: {
@@ -34,13 +36,34 @@ const styles = {
     paddingLeft: 0,
     marginTop: 25,
   },
+  container: {
+    margin: 40,
+    marginTop: 50,
+    padding: 25,
+    lineHeight: '1.4em',
+    textAlign: 'center',
+  },
+  h4: {
+    textTransform: 'lowercase',
+    letterSpacing: '0.1em',
+    color: '#df8671',
+    marginTop: 0,
+  },
+  spanAbout: {
+    color: '#27c79a',
+    fontWeight: 600,
+  },
+  icon: {
+    width: 40,
+    height: 40,
+  },
+  p: {
+    fontWeight: 300,
+    fontSize: '1em',
+  },
 };
 
 export default class Home extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     return <div>
@@ -50,8 +73,31 @@ export default class Home extends React.Component {
           </h3>
         </Paper>
       <div>
-        <Paper>
-          <h4></h4>
+        <Paper style={ styles.container }>
+          <WhatIcon style={ styles.icon }/>
+          <h4 style={ styles.h4 }>What is it?</h4>
+          <p style={ styles.p }><span style={ styles.spanAbout }>gamefam </span>
+            is an app that can be used to connect with other gamers, share lists of games with them
+            that you want to buy/play together and therefore saving money.</p>
+        </Paper>
+        <Paper style={ styles.container }>
+          <UseIcon style={ styles.icon }/>
+          <h4 style={ styles.h4 }>How to use.</h4>
+          <p style={ styles.p }>In the section 'lists' you can<br/>
+            <span style={ styles.spanAbout }>add lists</span><br/>
+            <span style={ styles.spanAbout }>edit existing lists</span><br/>
+            and <span style={ styles.spanAbout }>add users to your lists.</span>
+          </p>
+          <p style={ styles.p }>If you click on one list <br/>
+            the games added to the list are displayed.<br/>
+            There you can <br/>
+            <span style={ styles.spanAbout }>add games</span><br/>
+            and <span style={ styles.spanAbout }>remove games </span><br/>
+            by clicking on them.
+          </p>
+          <p style={ styles.p }>If you share a list with other users<br/>
+            their list gets automatically updated.
+          </p>
         </Paper>
       </div>
     </div>;
